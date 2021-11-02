562639855934b661b9125dd6cea30b2d2a202134
Carlos Mauricio <carlos.mauricio@binmatter.com>
2021-11-01 18:24:55 -0600

    Git open cors

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const app = express();
 
 connectDB(); 
 
-const corsOptions = {
-	origin: process.env.FRONTEND_URL,
-	credentials:true,
-	optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+// 	origin: process.env.FRONTEND_URL,
+// 	credentials:true,
+// 	optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: true }));
+app.options("*", cors());
 
 app.use(express.json({extend: true}));
 const port = process.env.PORT || 4000;
