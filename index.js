be557dfc371046b3b140c88bcb7ea7e4d825dc10
Carlos Mauricio <cmdeveloper97@gmail.com>
2021-12-29 00:13:23 -0700

    Change: Backend cors

--- index.js
+++ index.js
@@ -6,30 +6,14 @@ const app = express();
 
 connectDB(); 
 
-// const corsOptions = {
-// 	origin: process.env.FRONTEND_URL,
-// 	credentials:true,
-// 	optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
-// app.use(cors({ credentials: true, origin: true }));
-// app.options("*", cors());
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-app.use(allowCrossDomain);
-
+const corsOptions = {
+	origin: process.env.FRONTEND_URL,
+	credentials:true,
+	optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: true }));
+app.options("*", cors()); 
 
 app.use(express.json({extend: true}));
 const port = process.env.PORT || 4000;
