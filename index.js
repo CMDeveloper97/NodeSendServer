4fb31291dceebf110345f6b2664ae0747517913a
Carlos Mauricio <cmdeveloper97@gmail.com>
2021-12-28 21:02:04 -0700

    Change: Cors Configuration

--- index.js
+++ index.js
@@ -15,12 +15,22 @@ connectDB();
 // app.use(cors({ credentials: true, origin: true }));
 // app.options("*", cors());
 
-app.use(
-    cors({
-        origin: process.env.FRONTEND_URL, 
-        credentials: true,
-    })
-);
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
+
 app.use(express.json({extend: true}));
 const port = process.env.PORT || 4000;
 
